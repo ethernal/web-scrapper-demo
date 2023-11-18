af270632eafe0dbfc460729a1c4298d67f79cc0b
Sebastian Pieczynski <testament777@gmail.com>
2023-11-18 15:38:02 +0100

    feat: 💄 styling for products

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const products = (await axios.get('http://localhost:3213/api/products')).data;
+      const products = (await axios.get('http://localhost:3213/api/products?price_lte=100')).data;
 
       console.log('products', products);
       setProducts(products);
@@ -29,15 +29,15 @@ function App() {
 
   return (
     <>
-      <div className='w-full grid grid-cols-3 gap-4'>
+      <div className='w-full grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 p-8'>
         {products?.map((data) =>  {
 
           const metadata = JSON.parse(data.data);
           return (
-          <div key={data.url}>
-            <h1>{metadata.name}</h1>
+          <div key={data.url} className='border-slate-300 bg-slate-600 border-2 p-4 flex flex-col gap-2'>
+            <h1 className='text-4xl font-bold self-center'>{metadata.name}</h1>
             <img src={metadata.image} alt={metadata.name} />
-            <p>{metadata.currency}{data.price}</p>
+            <p className='bg-slate-300 text-black p-2 inline-block rounded-lg self-start font-bold text-xl tracking-wide'>{metadata.currency}{data.price}</p>
           </div>
         )
         }
